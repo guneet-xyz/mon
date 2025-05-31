73ab6cf23bdc549360ddd3594eed9f5b25f314f1
kvqn <guneetaggarwal@gmail.com>
2025-05-31 20:02:49 +0530

    wip: dockerfile

--- apps/wall/next.config.js
+++ apps/wall/next.config.js
@@ -1,9 +1,15 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 /** @type {import("next").NextConfig} */
-const config = {}
+const config = {
+  output: "standalone",
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
+}
 
 export default config
