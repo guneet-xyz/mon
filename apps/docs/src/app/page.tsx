191b4616ef7b6b569be6e9c7cf562418f3ac9ab4
kvqn <guneetaggarwal@gmail.com>
2025-06-26 16:39:28 +0530

    style: mobile view

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
   return (
     <div className="relative flex min-h-[calc(100vh-180px)] flex-col items-center justify-center">
       <div className="absolute inset-0 bg-radial from-green-500 to-neutral-100 opacity-20 blur-3xl dark:from-green-500 dark:to-neutral-950"></div>
-      <h1 className="z-10 pt-20 font-display text-4xl font-black md:text-6xl lg:text-8xl">
+      <h1 className="z-10 pt-10 font-display text-4xl font-black md:pt-20 md:text-6xl lg:text-8xl">
         MON
       </h1>
       <h2 className="px-8 py-4 text-center text-xl font-bold md:text-2xl lg:px-16 lg:text-3xl">
@@ -56,8 +56,8 @@ export default function HomePage() {
       </div>
       <div className="text-2xl font-bold">Why Mon?</div>
       <div className="px-8 text-center">{`First, it's a cool name. But if that doesn't convince you, here are some features.`}</div>
-      <div className="z-10 flex gap-8 px-20 py-8 *:flex-1">
-        <Card>
+      <div className="z-10 flex flex-wrap gap-4 px-8 py-8 *:flex-1 md:gap-8 md:px-20">
+        <Card className="min-w-50">
           <CardHeader>
             <CardTitle>One Config File</CardTitle>
           </CardHeader>
@@ -66,7 +66,7 @@ export default function HomePage() {
             clunky interfaces to set up your monitors.
           </CardContent>
         </Card>
-        <Card>
+        <Card className="min-w-50">
           <CardHeader>
             <CardTitle>Wide Support</CardTitle>
           </CardHeader>
@@ -74,7 +74,7 @@ export default function HomePage() {
             Supports all the protocols and services you might need to monitor.
           </CardContent>
         </Card>
-        <Card>
+        <Card className="min-w-50">
           <CardHeader>
             <CardTitle>And its beautiful</CardTitle>
           </CardHeader>
